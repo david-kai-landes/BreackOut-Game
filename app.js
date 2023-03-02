b2ec82a5e85f0e1172f88cca87437552f9d4f4d8
DavidKai-hub <67291503+DavidKai-hub@users.noreply.github.com>
2023-03-02 16:44:39 -0500

    added minor logic and styling

--- app.js
+++ app.js
@@ -1,12 +1,81 @@
-const grid = document.querySelector("grid");
-
-// Draw my Block
-function addBlocK() {
-  const block = document.createElement("div");
-  block.classList.add("block");
-  block.style.left = "100px";
-  block.style.left = "20px";
-  grid.appendChild(block);
+const grid = document.querySelector(".grid");
+const blockWidth = 100;
+const blockHeigth = 20;
+const boardWidth = 560;
+//
+const userStart = [230, 10];
+let currentPosition = userStart;
+//
+//Create Block
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topLeft = [xAxis, yAxis + blockHeigth];
+    this.topRight = [xAxis + blockWidth, yAxis];
+  }
 }
 //
-addBlocK();
+//all my blocks
+const blocks = [
+  new Block(10, 270),
+  new Block(120, 270),
+  new Block(230, 270),
+  new Block(340, 270),
+  new Block(450, 270),
+  new Block(10, 240),
+  new Block(120, 240),
+  new Block(230, 240),
+  new Block(340, 240),
+  new Block(450, 240),
+  new Block(10, 210),
+  new Block(120, 210),
+  new Block(230, 210),
+  new Block(340, 210),
+  new Block(450, 210),
+];
+//
+// Draw all my Blocks
+function addBlocks() {
+  for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.style.left = blocks[i].bottomLeft[0] + "px";
+    block.style.bottom = blocks[i].bottomLeft[1] + "px";
+    grid.appendChild(block);
+  }
+}
+//
+addBlocks();
+//
+//add user
+const user = document.createElement("div");
+user.classList.add("user");
+drawUser();
+grid.appendChild(user);
+//
+// Draw User
+function drawUser() {
+  user.style.left = currentPosition[0] + "px";
+  user.style.bottom = currentPosition[1] + "px";
+}
+//
+//move user
+function moveUser(e) {
+  switch (e.key) {
+    case "ArrowLeft":
+      if (currentPosition[0] > 0) {
+        currentPosition[0] -= 10;
+        drawUser();
+      }
+      break;
+    case "ArrowRight":
+      if (currentPosition[0] < boardWidth - blockWidth) {
+        currentPosition[0] += 10;
+        drawUser();
+      }
+      break;
+  }
+}
+//
+document.addEventListener("keydown", moveUser);
